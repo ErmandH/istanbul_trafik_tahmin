62f9a87141c6ded65f073ce5e27ad6f086aea24e
Ermand Haruni <88191097+ErmandH@users.noreply.github.com>
2025-04-11 15:45:03 +0300

    allowed host güncellemesi

--- trafik_tahmin/settings.py
+++ trafik_tahmin/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'django-insecure-1234567890abcdefghijklmnopqrstuvwxyz'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['165.227.133.219', 'istanbultrafiksistemi.site', 'www.istanbultrafiksistemi.site', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['45.147.47.221', 'istanbultrafiksistemi.site', 'www.istanbultrafiksistemi.site', 'localhost', '127.0.0.1']
 
 # Application definition
 
